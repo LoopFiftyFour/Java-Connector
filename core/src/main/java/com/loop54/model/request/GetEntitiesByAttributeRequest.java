cb40d61caad9758321053b7784a3dd7b046f7e30
Joel W Kall <joel@loop54.com>
2023-01-25 16:32:48 +0100

    Added support for RequestAlias

--- core/src/main/java/com/loop54/model/request/GetEntitiesByAttributeRequest.java
+++ core/src/main/java/com/loop54/model/request/GetEntitiesByAttributeRequest.java
@@ -9,12 +9,22 @@ public class GetEntitiesByAttributeRequest extends Request {
         attribute.name = attributeName;
         attribute.value = attributeValue;
     }
+    
+    public GetEntitiesByAttributeRequest(String attributeName, String attributeValue, RequestAliasData requestAlias) {
+        this(attributeName, attributeValue);
+        this.requestAlias = requestAlias;
+    }
 
     /**
      * The attribute name-value-pair to find entities connected to. Note: this attribute needs to be indexed in the
      * engine. See the endpoint GetIndexedAttribute.
      */
     public final AttributeNameValuePair attribute = new AttributeNameValuePair();
+    
+    /**
+     * Provides human-readable labels in the Portal.
+     */
+    public RequestAliasData requestAlias = null;
 
     /**
      * Parameters for specifying which entities to retrieve. Such as filtering, faceting, sorting and paging.
---
+++ core/src/main/java/com/loop54/model/request/RequestAliasData.java
@@ -0,0 +1,11 @@
+package com.loop54.model.request;
+
+/** Provides human-readable labels in the Portal. */
+public class RequestAliasData {
+    /** Specify an alias for this attribute name. */
+    public String name;
+    /** Specify an alias for this attribute value. */
+    public String value;
+    /** Specify a more detailed description for this attribute name–value pair. Will be shown in the Portal. */
+    public String details;
+}
