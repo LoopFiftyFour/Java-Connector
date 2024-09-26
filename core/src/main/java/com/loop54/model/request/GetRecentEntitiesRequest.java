1e6395869bbaaf147537dc806e25cc897c04a1e0
Alena Mikheika <ext-a.mikheika@fact-finder.com>
2024-09-26 19:24:50 +0300

    Qg21A1yi: small fix in xml doc;

--- core/src/main/java/com/loop54/model/request/GetRecentEntitiesRequest.java
+++ core/src/main/java/com/loop54/model/request/GetRecentEntitiesRequest.java
@@ -2,14 +2,12 @@ package com.loop54.model.request;
 
 import java.util.List;
 
-import com.loop54.Loop54Client;
-
 /** This class used to perform a request to get entities that a user (or all users) most recently interacted with or navigated to. */
 public class GetRecentEntitiesRequest extends GetPopularOrRecentEntitiesRequest {
     /**
      * @param  behaviorType interaction or navigation type to include (such as {@literal click}, {@literal purchase} or {@literal search}).
      * @param  forUserId    a user ID to retrieve the most common/recent entities for that user or null to retrieve the globally most common/recent entities.     
-     * {@link Loop54Client#CURRENT_USER_PLACEHOLDER} can be specified to use the user ID from the User-Id request header.
+     * {@link GetPopularOrRecentEntitiesRequest#CURRENT_USER_PLACEHOLDER} can be specified to use the user ID from the User-Id request header.
      * @param  entityType   entity types to include (such as {@literal Product} or {@literal Query})
      */
     public GetRecentEntitiesRequest(String behaviorType, String forUserId, List<String> entityType) {
@@ -20,7 +18,7 @@ public class GetRecentEntitiesRequest extends GetPopularOrRecentEntitiesRequest 
      * Retrieve the globally most recent products for a specfic user
      * @param  behaviorType interaction or navigation type to include (such as {@literal click}, {@literal purchase} or {@literal search}).
      * @param  forUserId    a user ID to retrieve the most common/recent entities for that user or null to retrieve the globally most common/recent entities.
-     * {@link Loop54Client#CURRENT_USER_PLACEHOLDER} can be specified to use the user ID from the User-Id request header.
+     * {@link GetPopularOrRecentEntitiesRequest#CURRENT_USER_PLACEHOLDER} can be specified to use the user ID from the User-Id request header.
      */
     public GetRecentEntitiesRequest(String behaviorType, String forUserId) { super(behaviorType, forUserId, null); }
 
